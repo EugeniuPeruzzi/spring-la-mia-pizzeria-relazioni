0121f2726a2bdce6898559ac116438ee0ff79ca4
EugeniuPeruzzi <jaka1997@live.it>
2023-12-11 15:14:47 +0100

    Refactor IngredientsController and add new ingredient creation functionality, Exercise done

--- src/main/java/org/java/spring/controller/IngredientsController.java
+++ src/main/java/org/java/spring/controller/IngredientsController.java
@@ -3,12 +3,12 @@ package org.java.spring.controller;
 import java.util.List;
 
 import org.java.spring.db.pojo.Ingredient;
-import org.java.spring.db.pojo.Pizza;
 import org.java.spring.db.serv.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,9 +17,10 @@ public class IngredientsController {
 
 	@Autowired
 	private IngredientService ingredientService;
-	
+
+//	SHOW
 	@GetMapping("/ingredients")
-    public String createDiscount(Model model) {
+    public String showIngredients(Model model) {
         
         List<Ingredient> ingredients = ingredientService.findAll();
         
@@ -27,6 +28,32 @@ public class IngredientsController {
         
         return "ingredientShow";
     }
+	
+//	CREATE SHOW
+	@GetMapping("/ingredient/new")
+    public String createIngredients(Model model) {
+		
+		Ingredient ingredient = new Ingredient();
+		
+		
+		model.addAttribute("ingredient", ingredient);
+	        
+	        return "ingredientCreate";
+	    }
+	
+//	STORE INGREDIENT
+	@PostMapping("/ingredient/new")
+	public String storeIngredients(@ModelAttribute Ingredient ingredient, Model model) {
+	    
+	    ingredientService.save(ingredient);
+	    
+	    model.addAttribute("ingredient", ingredient);
+	        
+	    return "redirect:/ingredients";
+	}
+
+	
+	
 	
 //	DELETE
 	@PostMapping("/ingredient/delete/{id}")
